05bb333d82635785543e297871d4d24937757c64
hartika5 <hartika5@aalto.fi>
2023-11-08 10:38:09 +0200

    Added header files for building, residential, commercial, industrial and person classes. Buildings and person are still quite simplified versions and there are no profiles implemented. Also the destructors are not implemented.

---
+++ src/building.hpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <string>
+
+class Building {
+public:
+    Building(const std::string& buildingName, std::pair<int,int> location) :
+            buildingName_(buildingName), location_(location) {}
+
+    virtual ~Building();
+
+    const std::string GetName() const {
+        return buildingName_;
+    }
+    const std::pair<int,int> GetLocation() const {
+        return location_;
+    }
+
+    virtual void Display() const = 0;
+
+private:
+    std::string buildingName_;
+    std::pair<int,int> location_;
+        
+};
