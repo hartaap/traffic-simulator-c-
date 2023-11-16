096a93f20a0c5e601d5a0cd0c860d27a06dc860a
Jiri Simell <jiri.simell@gmail.com>
2023-11-16 19:09:51 +0200

    Added a function for testing that creates a small 2x2 city with one building

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,72 @@
 #include "city.hpp"
 
+/* 
+    For testing.
+    Creates a 2x2 city with roads on the edges and a building in the middle, with a single road connected to the building.
+    Adds one car to the city.
+
+     i1:(0,0) _____________ i2:(0,2)
+             |      r1     |
+           r3|             |
+             |______b1     |r5
+             |  r6         |
+           r4|             |
+             |_____________|
+     i3:(2,0)       r2      i4:(2,2)
+
+    Returns the created city.
+*/
+City CreateSmallCity() {
+
+    // Initialize the city
+    City c(2, 2);
+
+    // Create the roads
+    Road* r1 = new Road({0, 0}, {0, 2}, 10);
+    Road* r2 = new Road({2, 0}, {2, 2}, 10);    
+    Road* r3 = new Road({0, 0}, {1, 0}, 10);
+    Road* r4 = new Road({1, 0}, {2, 0}, 10);
+    Road* r5 = new Road({0, 2}, {2, 2}, 10);
+    Road* r6 = new Road({1, 0}, {1, 1}, 10);
+
+    // Create the building
+    Industrial* b1 = new Industrial("Murata", {1, 1});
+
+    // Create the intersections
+    Intersection* i1 = new Intersection({0, 0}, {r1, r3});
+    Intersection* i2 = new Intersection({0, 2}, {r1, r5});
+    Intersection* i3 = new Intersection({2, 0}, {r2, r4});
+    Intersection* i4 = new Intersection({2, 2}, {r2, r5});
+    Intersection* i5 = new Intersection({1, 0}, {r3, r4, r6});
+
+    // Create a car to the city
+    Car* c1 = new Car(0.0, 0.0);
+
+    // Add the objects to the city
+    c.AddRoad(r1);
+    c.AddRoad(r2);
+    c.AddRoad(r3);
+    c.AddRoad(r4);
+    c.AddRoad(r5);
+    c.AddRoad(r6);
+    c.AddBuilding(b1);    
+    c.AddIntersection(i1);
+    c.AddIntersection(i2);
+    c.AddIntersection(i3);
+    c.AddIntersection(i4);
+    c.AddIntersection(i5);
+    c.AddCar(c1);
+
+    return c;
+}
+
 int main() {
     // Create an SFML window
     sf::RenderWindow window(sf::VideoMode(800, 600), "City Simulation");
 
-    
-    City c(10, 10); 
+    City city = CreateSmallCity();
 
-    Road* r1 = new Road({0, 0}, {4, 0}, 10);
-    Road* r2 = new Road({1, 1}, {1, 5}, 10);
-    Road* r3 = new Road({1, 1}, {5, 1}, 10);
-
-    Car* c1 = new Car(0.0, 0.0);
-
-    Building* b1 = new Building("Murata", {4, 2});
-    Building* b2 = new Building("ABB", {-1, -1});
-
-    c.AddRoad(r1);
-    c.AddRoad(r2);
-    c.AddRoad(r3);
-    c.AddBuilding(b1);
-    c.AddBuilding(b2);
-    c.AddCar(c1);
-
-     sf::Clock clock;
+    sf::Clock clock;
 
     // Main loop
     while (window.isOpen()) {
@@ -38,11 +81,11 @@ int main() {
         window.clear();
 
         //Move cars
-        c.UpdateCars(deltaTime);
+        city.UpdateCars(deltaTime);
 
         //Draw the city and the cars
-        c.PrintCity(window);
-        c.DrawCars(window);
+        city.PrintCity(window);
+        city.DrawCars(window);
 
         window.display();
     }
