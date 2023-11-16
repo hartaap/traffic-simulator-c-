e0bb617b704d8d96be5383029c5d21de2915bc23
Jiri Simell <jiri.simell@gmail.com>
2023-11-16 19:00:14 +0200

    Added 'AddIntersection' and 'AddTrafficLight' functions

--- src/city.hpp
+++ src/city.hpp
@@ -7,6 +7,8 @@
 #include "road.hpp"
 #include "building.hpp"
 #include "car.hpp"
+#include "trafficlight.hpp"
+#include "intersection.hpp"
 #include <vector>
 
 
@@ -18,21 +20,25 @@ class City{
 
     ~City();
 
-   bool IsValidRoad(Road* r) const;
+    bool IsValidRoad(Road* r) const;
 
-   void AddRoad(Road* r);
+    void AddRoad(Road* r);
 
-   bool IsValidBuilding(Building* b) const;
+    bool IsValidBuilding(Building* b) const;
 
-   void AddBuilding(Building* b);
+    void AddBuilding(Building* b);
 
-   void AddCar(Car* c);
+    void AddCar(Car* c);
 
-   void UpdateCars(float deltaTime);
+    void AddIntersection(Intersection* i);
 
-   void DrawCars(sf::RenderWindow& window);
+    void AddTrafficLight(TrafficLight* t);
 
-   void PrintCity(sf::RenderWindow& window);
+    void UpdateCars(float deltaTime);
+
+    void DrawCars(sf::RenderWindow& window);
+
+    void PrintCity(sf::RenderWindow& window);
 
 
 
@@ -41,6 +47,8 @@ class City{
     Grid* grid_;
     std::vector<Road*> roads_;
     std::vector<Building*> buildings_;
+    std::vector<Intersection*> intersections_;
+    std::vector<TrafficLight*> trafficLights_;
     std::vector<Car*> cars_;
     
 };
