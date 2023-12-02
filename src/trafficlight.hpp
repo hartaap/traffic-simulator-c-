67c212ee20979422de924701f9048c02575990e6
Jiri Simell <jiri.simell@gmail.com>
2023-12-03 01:33:22 +0200

    Added boolean variables that describe the current color of the light, and modifier functions for these variables

--- src/trafficlight.hpp
+++ src/trafficlight.hpp
@@ -5,24 +5,55 @@
 
 class TrafficLight {
  public:
-  TrafficLight(std::pair<int, int> location, int redDuration, int greenDuration)
+  // Constructor
+  TrafficLight(std::pair<int, int> location, int redDuration,
+               int yellowDuration, int greenDuration)
       : location_(location),
         redDuration_(redDuration),
-        greenDuration_(greenDuration) {};
+        yellowDuration_(yellowDuration),
+        greenDuration_(greenDuration){};
 
+  // Returns the coordinates of the traffic light's location
   std::pair<int, int> GetLocation() const { return location_; }
 
+  // Returns the duration that the traffic light stays green
   int GetGreenDuration() const { return greenDuration_; }
 
+  // Returns the duration that the traffic light stays red
   int GetRedDuration() const { return redDuration_; }
 
+  // Returns the duration that the traffic light stays yellow
   int GetYellowDuration() const { return yellowDuration_; }
+
+  // Switches the state of the traffic light to red
+  void TurnRed() {
+    isRed_ = true;
+    isYellow_ = false;
+    isGreen_ = false;
+  }
+
+  // Switches the state of the traffic light to yellow
+  void TurnYellow() {
+    isRed_ = false;
+    isYellow_ = true;
+    isGreen_ = false;
+  }
+
+  // Switches the state of the traffic light to green
+  void TurnGreen() {
+    isRed_ = false;
+    isYellow_ = false;
+    isGreen_ = true;
+  }
 
  private:
   std::pair<int, int> location_;
   int redDuration_;
   int greenDuration_;
-  int yellowDuration_ = 1; // Same for all TrafficLight instances
+  int yellowDuration_;
+  bool isRed_ = false;
+  bool isYellow_ = false;
+  bool isGreen_ = true;  // In the beginning, the light is green by default
 };
 
 #endif
