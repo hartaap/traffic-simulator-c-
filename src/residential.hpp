c62b6a3af25174331060ebfc5bb274a84bfb0cdd
Hartikainen Aape <aape.hartikainen@aalto.fi>
2023-12-03 16:36:21 +0200

    Small change to clean the code

--- src/residential.hpp
+++ src/residential.hpp
@@ -1,19 +1,20 @@
-#ifndef INDUSTRIAL_H
-#define INDUSTRIAL_H
+#ifndef RESIDENTIAL_H
+#define RESIDENTIAL_H
 
 #include "building.hpp"
 
-class Industrial : public Building {
+class Residential : public Building {
  public:
-    Industrial(const std::string& buildingName, const std::pair<int, int>& location)
+    Residential(const std::string& buildingName, const std::pair<int, int>& location)
     : Building(buildingName, location) {
-        maxPersonCapacity_ = 30;
-        maxCarCapacity_ = 25;
+        maxPersonCapacity_ = 5;
+        maxCarCapacity_ = 2;
     }
-    
+
     void Info() const override {
-        std::cout << GetName() << " is an industrial building!" << std::endl;
+        std::cout << GetName() << " is a residential building!" << std::endl;
     }
+
 };
 
 #endif
