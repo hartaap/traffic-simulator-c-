cbe3fe8d4d02201ca33d646a542fbfbcc37d1129
Hartikainen Aape <aape.hartikainen@aalto.fi>
2023-12-03 16:35:14 +0200

    Small change to clean the code

--- src/residential.hpp
+++ src/residential.hpp
@@ -6,21 +6,15 @@
 class Residential : public Building {
  public:
     Residential(const std::string& buildingName, const std::pair<int, int>& location)
-    : Building(buildingName, location), maxPeopleCapacity_(5), maxCarCapacity_(2) {}
+    : Building(buildingName, location) {
+        maxPersonCapacity_ = 5;
+        maxCarCapacity_ = 2;
+    }
 
-    int GetMaxPeopleCapacity() const override{
-        return maxPeopleCapacity_;
-    }
-    int GetMaxCarCapacity() const override {
-        return maxCarCapacity_;
-    }
     void Info() const override {
         std::cout << GetName() << " is a residential building!" << std::endl;
     }
 
- private:
-    int maxPeopleCapacity_;
-    int maxCarCapacity_;
 };
 
 #endif
