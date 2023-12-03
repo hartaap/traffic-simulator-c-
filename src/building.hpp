49a6ce5c6a4d97fdbcf74c1c0df1057d23cf438b
Hartikainen Aape <aape.hartikainen@aalto.fi>
2023-12-03 15:37:56 +0200

    Added function to acquire information about current state of building, i.e. is it full or not.

--- src/building.hpp
+++ src/building.hpp
@@ -1,10 +1,10 @@
 #ifndef BUILDING_H
 #define BUILDING_H
 
-#include "person.hpp"
-#include "car.hpp"
 #include <vector>
 #include <string>
+#include <iostream>
+
 
 class Building {
  public:
@@ -16,10 +16,16 @@ class Building {
 
     virtual int GetMaxCarCapacity() const = 0; 
 
+    virtual void Info() const = 0;
+
     int GetPeopleAmount() const;
 
     int GetCarAmount() const;
 
+    bool isFull() const {
+      return ((maxPersonCapacity_ - personAmount_) > 0);
+    }
+    
     const std::string& GetName() const;
 
     const std::pair<int, int>& GetLocation() const;
@@ -27,8 +33,11 @@ class Building {
  private:
     std::string buildingName_;
     std::pair<int, int> location_;
-    std::vector<Person> persons_;
-    std::vector<Car> cars_;
+    int personAmount_;
+    int carAmount_;
+    int maxPersonCapacity_;
+    int maxCarCapacity_;
 };
 
 #endif
+
