6c9714efad10cec9d50660c8f70f6b1c9d9926de
Hartikainen Aape <aape.hartikainen@aalto.fi>
2023-12-03 19:07:48 +0200

    Little changes so the addbuilding works more conveniently

--- src/commercial.cpp
+++ src/commercial.cpp
@@ -1,34 +1,39 @@
 #include "commercial.hpp"
 
-Commercial::Commercial(const std::string& buildingName, const std::pair<int, int>& location, CommercialType type)
-    : Building(buildingName, location), type_(type) {
+Commercial::Commercial(const std::string& buildingName, const std::pair<int, int>& location, const std::string& type)
+    : Building(buildingName, location, type), type_(type) {
     
-    switch (type) {
-        case GYM:
-            maxPersonCapacity_ = 25;
-            maxCarCapacity_ = 10;
-            break;
-        case RESTAURANT:
-            maxPersonCapacity_ = 30;
-            maxCarCapacity_ = 8;
-            break;
-        case SHOP:
-            maxPersonCapacity_ = 35;
-            maxCarCapacity_ = 30;
-            break;
-        default:
-            throw std::invalid_argument("Unvalid type");
+    std::string lowertype = type;
+    // Compare two strings in a case-insensitive manner
+    std::transform(lowertype.begin(), lowertype.end(), lowertype.begin(), [](unsigned char c){ return std::tolower(c); });
+
+    if (lowertype == "gym") {
+        maxPersonCapacity_ = 25;
+        maxCarCapacity_ = 10;
+    } else if (lowertype == "restaurant") {
+        maxPersonCapacity_ = 30;
+        maxCarCapacity_ = 8;
+    } else if (lowertype == "shop") {
+        maxPersonCapacity_ = 35;
+        maxCarCapacity_ = 30;
+    } else {
+        throw std::invalid_argument("Invalid type");
     }
 }
 
 void Commercial::Info() const {
-        std::cout << GetName() << " is a perfect place for ";
-        if (type_ == GYM) {
+    std::string lowertype = type_;
+    // Compare two strings in a case-insensitive manner again
+    std::transform(lowertype.begin(), lowertype.end(), lowertype.begin(), [](unsigned char c){ return std::tolower(c); });
+
+    std::cout << GetName() << " is a perfect place for ";
+        if (lowertype == "gym") {
             std::cout << "training!" << std::endl;
-         } else if (type_== RESTAURANT) {
+         } else if (lowertype == "restaurant") {
             std::cout << "eating!" << std::endl;
          } else {
             std::cout << "shopping!" << std::endl;
          }
     }
+
 
