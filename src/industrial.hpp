fb495cdf24ffc62ce471e5b7c0df1e650a01af54
Hartikainen Aape <aape.hartikainen@aalto.fi>
2023-12-03 16:36:49 +0200

    Small change to clean the code

--- src/industrial.hpp
+++ src/industrial.hpp
@@ -6,21 +6,14 @@
 class Industrial : public Building {
  public:
     Industrial(const std::string& buildingName, const std::pair<int, int>& location)
-    : Building(buildingName, location), maxPeopleCapacity_(30), maxCarCapacity_(25) {}
+    : Building(buildingName, location) {
+        maxPersonCapacity_ = 30;
+        maxCarCapacity_ = 25;
+    }
     
-    int GetMaxPeopleCapacity() const override {
-        return maxPeopleCapacity_;
-    }
-    int GetMaxCarCapacity() const override {
-        return maxCarCapacity_;
-    }
     void Info() const override {
         std::cout << GetName() << " is an industrial building!" << std::endl;
     }
-
- private:
-    int maxPeopleCapacity_;
-    int maxCarCapacity_;
 };
 
 #endif
