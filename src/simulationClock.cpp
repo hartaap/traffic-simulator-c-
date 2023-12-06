f92b4d3d6e51090625bbbb4a068cc3ed871c366f
aba <brander.artur@gmail.com>
2023-12-06 15:47:30 +0200

    User can now see city's time with the 'status' command. Simplified simulator...

--- src/simulationClock.cpp
+++ src/simulationClock.cpp
@@ -11,3 +11,19 @@ double SimulationClock::GetElapsedTime() {
     auto elapsedTime = std::chrono::duration_cast<std::chrono::duration<double>>(currentTime - startTime_);
     return elapsedTime.count();
 }
+
+std::string SimulationClock::GetSimulationTime() {
+    // 1 second in real-time equals 1 minute in simulation time
+    double elapsedMinutes = GetElapsedTime();
+
+    // Convert elapsed minutes to hours and minutes
+    int hours = static_cast<int>(elapsedMinutes) / 60;
+    int minutes = static_cast<int>(elapsedMinutes) % 60;
+
+    // Format the time as "hours:minutes"
+    std::stringstream ss;
+    ss << std::setw(2) << std::setfill('0') << hours << ":"
+       << std::setw(2) << std::setfill('0') << minutes;
+
+    return ss.str();
+}
