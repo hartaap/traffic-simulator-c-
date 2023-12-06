6cbb6534576fc36073c46b0d7f14931ac2616535
aba <brander.artur@gmail.com>
2023-12-06 15:52:04 +0200

    If hours exceed 24, the clock resets to 00:00

--- src/simulationClock.cpp
+++ src/simulationClock.cpp
@@ -7,23 +7,29 @@ void SimulationClock::Start() {
 }
 
 double SimulationClock::GetElapsedTime() {
-    auto currentTime = std::chrono::high_resolution_clock::now();
-    auto elapsedTime = std::chrono::duration_cast<std::chrono::duration<double>>(currentTime - startTime_);
-    return elapsedTime.count();
+  auto currentTime = std::chrono::high_resolution_clock::now();
+  auto elapsedTime = std::chrono::duration_cast<std::chrono::duration<double>>(
+      currentTime - startTime_);
+  return elapsedTime.count();
 }
 
 std::string SimulationClock::GetSimulationTime() {
-    // 1 second in real-time equals 1 minute in simulation time
-    double elapsedMinutes = GetElapsedTime();
+  // 1 second in real-time equals 1 minute in simulation time
+  double elapsedMinutes = GetElapsedTime();
 
-    // Convert elapsed minutes to hours and minutes
-    int hours = static_cast<int>(elapsedMinutes) / 60;
-    int minutes = static_cast<int>(elapsedMinutes) % 60;
+  // Convert elapsed minutes to hours and minutes
+  int hours = static_cast<int>(elapsedMinutes) / 60;
+  int minutes = static_cast<int>(elapsedMinutes) % 60;
 
-    // Format the time as "hours:minutes"
-    std::stringstream ss;
-    ss << std::setw(2) << std::setfill('0') << hours << ":"
-       << std::setw(2) << std::setfill('0') << minutes;
+  if (hours >= 24) {
+    hours = 0;
+    minutes = 0;
+  }
 
-    return ss.str();
+  // Format the time as "hours:minutes"
+  std::stringstream ss;
+  ss << std::setw(2) << std::setfill('0') << hours << ":" << std::setw(2)
+     << std::setfill('0') << minutes;
+
+  return ss.str();
 }
