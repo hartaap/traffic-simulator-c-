b8afe3783c205ea0ffa95d1c28e84910d858edd6
Jiri Simell <jiri.simell@gmail.com>
2023-12-07 21:05:34 +0200

    Minor changes to the documentation

--- src/road.hpp
+++ src/road.hpp
@@ -7,7 +7,7 @@ class Road {
  public:
   /**
    * @brief Construct a new Road object.
-   * 
+   *
    * @param start The starting coordinates of the road
    * @param end The ending coordinates of the road
    * @param speedLimit The speed limit on the road
@@ -16,35 +16,35 @@ class Road {
 
   /**
    * @brief Get the coordinates of the end of the road.
-   * 
-   * @return std::pair<int, int> 
+   *
+   * @return std::pair<int, int>
    */
   std::pair<int, int> GetEnd() const;
 
   /**
    * @brief Get the coordinates of the start of the road.
-   * 
-   * @return std::pair<int, int> 
+   *
+   * @return std::pair<int, int>
    */
   std::pair<int, int> GetStart() const;
 
   /**
-   * @brief Get the speed limit of the road
-   * 
+   * @brief Get the speed limit of the road.
+   *
    * @return The speed limit as an integer
    */
   int GetSpeedLimit() const;
 
   /**
    * @brief Returns a boolean value indicating if the road os vertical or not.
-   * 
+   *
    * @return True if the road is vertical, false otherwise
    */
   bool IsVertical() const;
 
   /**
    * @brief Returns a boolean value indicating if the road os horizontal or not.
-   * 
+   *
    * @return True if the road is horizontal, false otherwise
    */
   bool IsHorizontal() const;
