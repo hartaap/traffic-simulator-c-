369ab1637ac61cce0debf85bcc209141456ef916
Jiri Simell <jiri.simell@gmail.com>
2023-12-07 20:45:48 +0200

    Added documentation for the Residential, Commercial, and Industrial classes

--- src/commercial.hpp
+++ src/commercial.hpp
@@ -1,19 +1,37 @@
 #ifndef COMMERCIAL_H
 #define COMMERCIAL_H
 
-#include "building.hpp"
 #include <algorithm>
 
+#include "building.hpp"
+
+/**
+ * @brief One of the concretizing classes for the abstract class Building.
+ *
+ */
 class Commercial : public Building {
  public:
-    Commercial(const std::string& buildingName, const std::pair<int, int>& location, const std::string& commercialType);
+  /**
+   * @brief Construct a new Commercial object
+   *
+   * @param buildingName The name of the commercial building
+   * @param location The coordinates of the commercial building
+   * @param commercialType The type of the commercial building ("gym",
+   * "restaurant", or "shop")
+   */
+  Commercial(const std::string& buildingName,
+             const std::pair<int, int>& location,
+             const std::string& commercialType);
 
-    void Info() const override;
+  /**
+   * @brief Prints information about the commercial building to the standard
+   * output
+   *
+   */
+  void Info() const override;
 
  private:
-    const std::string type_;
-   
+  const std::string type_;
 };
 
 #endif
-
