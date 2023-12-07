71ebce0ef4f2c2df52f15652a411981253ead478
Jiri Simell <jiri.simell@gmail.com>
2023-12-07 21:07:25 +0200

    Styling

--- src/trafficlight.hpp
+++ src/trafficlight.hpp
@@ -1,19 +1,18 @@
 #ifndef TRAFFICLIGHT_H
 #define TRAFFICLIGHT_H
 
-#include <utility>
 #include <iostream>
-
+#include <utility>
 
 /**
  * @brief A class for representing the traffic lights in the simulated city.
- * 
+ *
  */
 class TrafficLight {
  public:
   /**
    * @brief Construct a new Traffic Light object.
-   * 
+   *
    * @param location The coordinates of the traffic light
    * @param redDuration The duration of the red light
    * @param yellowDuration The duration of the yellow light
@@ -28,37 +27,35 @@ class TrafficLight {
 
   /**
    * @brief Get the coordinates of the traffic light's location.
-   * 
-   * @return The coordinates of the traffic light as std::pair<int, int> 
+   *
+   * @return The coordinates of the traffic light as std::pair<int, int>
    */
   std::pair<int, int> GetLocation() const { return location_; }
 
-
-
   /**
    * @brief Get the red light duration of the traffic light.
-   * 
+   *
    * @return Red light duration
    */
   int GetRedDuration() const { return redDuration_; }
 
   /**
    * @brief Get the yellow light duration of the traffic light.
-   * 
+   *
    * @return Yellow light duration
    */
   int GetYellowDuration() const { return yellowDuration_; }
 
   /**
    * @brief Get the green light duration of the traffic light.
-   * 
+   *
    * @return Green light duration
    */
   int GetGreenDuration() const { return greenDuration_; }
-  
+
   /**
    * @brief Switches the state of the traffic light to red.
-   * 
+   *
    */
   void TurnRed() {
     isRed_ = true;
@@ -68,7 +65,7 @@ class TrafficLight {
 
   /**
    * @brief Switches the state of the traffic light to yellow.
-   * 
+   *
    */
   void TurnYellow() {
     isRed_ = false;
@@ -78,7 +75,7 @@ class TrafficLight {
 
   /**
    * @brief Switches the state of the traffic light to green.
-   * 
+   *
    */
   void TurnGreen() {
     isRed_ = false;
@@ -88,8 +85,9 @@ class TrafficLight {
 
   /**
    * @brief Get the current color of the traffic light.
-   * 
-   * @return A string representing the current state of the traffic light ("red", "yellow", or "green")
+   *
+   * @return A string representing the current state of the traffic light
+   * ("red", "yellow", or "green")
    */
   std::string GetStatus() const {
     if (isRed_) {
@@ -104,7 +102,7 @@ class TrafficLight {
  private:
   std::pair<int, int> location_;
   int redDuration_;
-  int yellowDuration_;  
+  int yellowDuration_;
   int greenDuration_;
   bool isRed_ = false;
   bool isYellow_ = false;
