0dcc5bb7ab2d529e0df75b71e27c5e4c0a171c89
Jiri Simell <jiri.simell@gmail.com>
2023-12-07 13:48:17 +0200

    Improved the documentation. Created a cpp file for the Intersection class and moved function definitions there.

--- src/intersection.hpp
+++ src/intersection.hpp
@@ -3,8 +3,14 @@
 
 #include <SFML/Graphics.hpp>
 #include <vector>
-#include "grid.hpp"
 
+#include "grid.hpp"
+#include "trafficlight.hpp"
+
+/**
+ * @brief A class used for representing an intersection in the simulated city.
+ * 
+ */
 class Intersection {
  public:
   /**
@@ -12,15 +18,14 @@ class Intersection {
    *
    * @param location the coordinates of the intersection as std::pair<int, int>
    */
-  Intersection(std::pair<int, int> location)
-      : location_(location), allowVertical_(true), allowHorizontal_(true) {}
+  Intersection(std::pair<int, int> location);
 
   /**
    * @brief Get the location of the intersection.
    *
    * @return The coordinates of the intersection as std::pair<int, int>
    */
-  std::pair<int, int> GetLocation() const { return location_; }
+  std::pair<int, int> GetLocation() const;
 
   /**
    * @brief Attempts to update the status of the traffic lights in the
@@ -28,121 +33,40 @@ class Intersection {
    *
    * @param deltaTime
    */
-  void Update(float deltaTime) {
-    if (trafficLight_ != nullptr) {
-      timePassed_ = timePassed_ + deltaTime;
-
-      if (yellow_) {
-        if (timePassed_ >= trafficLight_->GetYellowDuration()) {
-          if (allowVertical_) {
-            allowVertical_ = false;
-            allowHorizontal_ = true;
-          } else {
-            allowHorizontal_ = false;
-            allowVertical_ = true;
-          }
-          yellow_ = false;
-          timePassed_ = 0;
-        }
-      } else {
-        if (timePassed_ >= trafficLight_->GetGreenDuration()) {
-          yellow_ = true;
-          timePassed_ = 0;
-        }
-      }
-    }
-  }
+  void Update(float deltaTime);
 
   /**
    * @brief Adds a traffic light to the intersection.
    *
    * @param t A pointer to a TrafficLight object
    */
-  void AddTrafficLight(TrafficLight* t) { trafficLight_ = t; }
+  void AddTrafficLight(TrafficLight* t);
 
   /**
-   * @brief Returns a boolean value indicating if cars are currently allowed to move vertically through the intersection
-   * 
-   * @return true 
-   * @return false 
+   * @brief Returns a boolean value indicating if cars are currently allowed to
+   * move vertically through the intersection.
+   *
+   * @return true
+   * @return false
    */
-  bool AllowVertical() { return allowVertical_; }
+  bool AllowVertical();
 
   /**
-   * @brief Returns a boolean value indicating if cars are currently allowed to move horizontally through the intersection
-   * 
-   * @return true 
-   * @return false 
+   * @brief Returns a boolean value indicating if cars are currently allowed to
+   * move horizontally through the intersection.
+   *
+   * @return true
+   * @return false
    */
-  bool AllowHorizontal() { return allowHorizontal_; }
+  bool AllowHorizontal();
 
   /**
    * @brief Draws the intersection into the SFML window given as parameter.
-   * 
+   *
    * @param window The SFML window to draw the intersection to
    * @param cellSize The size of a single cell in the window
    */
-  void Draw(sf::RenderWindow& window, int cellSize, Grid* grid) {
-    sf::Color Gray(50, 50, 50);
-
-    // Draw the cell
-    sf::RectangleShape cellShape(sf::Vector2f(cellSize, cellSize));
-    cellShape.setPosition(location_.first * cellSize,
-                          location_.second * cellSize);
-    cellShape.setFillColor(Gray);
-    window.draw(cellShape);
-
-    if (trafficLight_ != nullptr) {
-      // Shapes for the traffic lights
-      sf::RectangleShape up(sf::Vector2f(10, 10));
-      sf::RectangleShape down(sf::Vector2f(10, 10));
-      sf::RectangleShape left(sf::Vector2f(10, 10));
-      sf::RectangleShape right(sf::Vector2f(10, 10));
-
-      if (yellow_) {
-        up.setFillColor(sf::Color::Yellow);
-        down.setFillColor(sf::Color::Yellow);
-        left.setFillColor(sf::Color::Yellow);
-        right.setFillColor(sf::Color::Yellow);
-      } else if (allowHorizontal_) {
-        up.setFillColor(sf::Color::Red);
-        down.setFillColor(sf::Color::Red);
-        left.setFillColor(sf::Color::Green);
-        right.setFillColor(sf::Color::Green);
-      } else {
-        up.setFillColor(sf::Color::Green);
-        down.setFillColor(sf::Color::Green);
-        left.setFillColor(sf::Color::Red);
-        right.setFillColor(sf::Color::Red);
-      }
-
-      up.setPosition(location_.first * cellSize,
-                     location_.second * cellSize - 10);
-      down.setPosition(location_.first * cellSize + (cellSize - 10),
-                       location_.second * cellSize + cellSize);
-      left.setPosition(location_.first * cellSize - 10,
-                       location_.second * cellSize + (cellSize - 10));
-      right.setPosition(location_.first * cellSize + cellSize,
-                        location_.second * cellSize);
-
-      if(grid->GetCell(location_.first, location_.second-1)->GetType().find("Road") != std::string::npos){
-         window.draw(up);
-      }
-
-      if(grid->GetCell(location_.first, location_.second+1)->GetType().find("Road") != std::string::npos){
-         window.draw(down);
-      }
-
-      if(grid->GetCell(location_.first-1, location_.second)->GetType().find("Road") != std::string::npos){
-         window.draw(left);
-      }
-
-      if(grid->GetCell(location_.first+1, location_.second)->GetType().find("Road") != std::string::npos){
-         window.draw(right);
-      }
-
-    }
-  }
+  void Draw(sf::RenderWindow& window, int cellSize, Grid* grid);
 
  private:
   std::pair<int, int> location_;
