a87c4e971d19d87f84435d410b3c63051994d905
Jiri Simell <jiri.simell@gmail.com>
2023-12-07 20:23:11 +0200

    Added documentation for the Cell class

--- src/cell.cpp
+++ src/cell.cpp
@@ -1,82 +1,73 @@
 #include "cell.hpp"
 
+Cell::Cell(std::pair<int, int> location)
+    : type_("Empty"), location_(location) {}
 
-
-
-bool Cell::IsOccupied() const {
-
-    return (type_ != "Empty");
-}
-
+bool Cell::IsOccupied() const { return (type_ != "Empty"); }
 
 void Cell::Occupy(std::string type) {
-
-    type_ = type;;
+  type_ = type;
+  ;
 }
 
+void Cell::Clear() { type_ = "Empty"; }
 
-void Cell::Clear() {
+std::string Cell::GetType() const { return type_; }
 
-    type_ = "Empty";
-}
+int Cell::GetX() const { return location_.first; }
 
+int Cell::GetY() const { return location_.second; }
 
-std::string Cell::GetType()  {
+void Cell::Draw(sf::RenderWindow& window, int cellSize, int x, int y) {
+  sf::RectangleShape cellShape(sf::Vector2f(cellSize, cellSize));
 
-    return type_;
-}
+  sf::Color Gray(50, 50, 50);
 
-void Cell::Draw(sf::RenderWindow& window, int cellSize, int x, int y){
+  if (type_ == "Horizontal Road") {
+    cellShape.setFillColor(Gray);
+    sf::RectangleShape dot(sf::Vector2f(cellSize / 2.0f, 2.0f));
+    dot.setFillColor(sf::Color::White);
+    dot.setPosition(x * cellSize, y * cellSize + cellSize / 2.0f);
+    cellShape.setPosition(x * cellSize, y * cellSize);
+    window.draw(cellShape);
+    window.draw(dot);
 
-    sf::RectangleShape cellShape(sf::Vector2f(cellSize,cellSize));
+  } else if (type_ == "Vertical Road") {
+    cellShape.setFillColor(Gray);
+    sf::RectangleShape dot(sf::Vector2f(2.0f, cellSize / 2.0f));
+    dot.setFillColor(sf::Color::White);
+    dot.setPosition(x * cellSize + cellSize / 2.0f, y * cellSize);
+    cellShape.setPosition(x * cellSize, y * cellSize);
+    window.draw(cellShape);
+    window.draw(dot);
 
-    sf::Color Gray(50, 50, 50);
-
-    if(type_ == "Horizontal Road"){
-        cellShape.setFillColor(Gray);
-        sf::RectangleShape dot(sf::Vector2f(cellSize / 2.0f, 2.0f));
-        dot.setFillColor(sf::Color::White);
-        dot.setPosition(x*cellSize, y*cellSize + cellSize/2.0f);
-        cellShape.setPosition(x *cellSize, y * cellSize); 
-        window.draw(cellShape);
-        window.draw(dot);
-
-    }else if(type_ == "Vertical Road"){
-        cellShape.setFillColor(Gray);
-        sf::RectangleShape dot(sf::Vector2f(2.0f, cellSize / 2.0f));
-        dot.setFillColor(sf::Color::White);
-        dot.setPosition(x*cellSize + cellSize/2.0f, y*cellSize);
-        cellShape.setPosition(x *cellSize, y * cellSize); 
-        window.draw(cellShape);
-        window.draw(dot);
-
-    }else if(type_ == "industrial"){
-        cellShape.setFillColor(sf::Color::Black);
-        cellShape.setPosition(x *cellSize, y * cellSize); 
-        window.draw(cellShape);
-    }else if(type_ == "residential"){
-        cellShape.setFillColor(sf::Color::Magenta);
-        cellShape.setPosition(x *cellSize, y * cellSize); 
-        window.draw(cellShape);
-    }else if(type_ == "shop"){
-        cellShape.setFillColor(sf::Color::Green);
-        cellShape.setPosition(x *cellSize, y * cellSize); 
-        window.draw(cellShape);
-    }else if(type_ == "gym"){
-        cellShape.setFillColor(sf::Color::Cyan);
-        cellShape.setPosition(x *cellSize, y * cellSize); 
-        window.draw(cellShape);
-    }else if(type_ == "restaurant"){
-        cellShape.setFillColor(sf::Color::Red);
-        cellShape.setPosition(x *cellSize, y * cellSize); 
-        window.draw(cellShape);
-    }else if(type_ == "Intersection"){
-        cellShape.setFillColor(Gray);
-        cellShape.setPosition(x*cellSize, y*cellSize);
-        window.draw(cellShape);
-    }else {
-        cellShape.setFillColor(sf::Color::White);
-        cellShape.setPosition(x *cellSize, y * cellSize); 
-        window.draw(cellShape); 
-    }
+  } else if (type_ == "industrial") {
+    cellShape.setFillColor(sf::Color::Black);
+    cellShape.setPosition(x * cellSize, y * cellSize);
+    window.draw(cellShape);
+  } else if (type_ == "residential") {
+    cellShape.setFillColor(sf::Color::Magenta);
+    cellShape.setPosition(x * cellSize, y * cellSize);
+    window.draw(cellShape);
+  } else if (type_ == "shop") {
+    cellShape.setFillColor(sf::Color::Green);
+    cellShape.setPosition(x * cellSize, y * cellSize);
+    window.draw(cellShape);
+  } else if (type_ == "gym") {
+    cellShape.setFillColor(sf::Color::Cyan);
+    cellShape.setPosition(x * cellSize, y * cellSize);
+    window.draw(cellShape);
+  } else if (type_ == "restaurant") {
+    cellShape.setFillColor(sf::Color::Red);
+    cellShape.setPosition(x * cellSize, y * cellSize);
+    window.draw(cellShape);
+  } else if (type_ == "Intersection") {
+    cellShape.setFillColor(Gray);
+    cellShape.setPosition(x * cellSize, y * cellSize);
+    window.draw(cellShape);
+  } else {
+    cellShape.setFillColor(sf::Color::White);
+    cellShape.setPosition(x * cellSize, y * cellSize);
+    window.draw(cellShape);
+  }
 }
--- src/cell.hpp
+++ src/cell.hpp
@@ -1,38 +1,79 @@
 #ifndef CELL_H
 #define CELL_H
 
-#include <string>
 #include <SFML/Graphics.hpp>
+#include <string>
 
+/**
+ * @brief A class for representing a single cell in the graphical simulation.
+ *
+ */
 class Cell {
-public:
+ public:
+  /**
+   * @brief Construct a new Cell object.
+   *
+   * @param location the coordinates of the cell
+   */
+  Cell(std::pair<int, int> location);
 
-    Cell(std::pair<int, int> location): type_("Empty"), location_(location){}
-    
-    bool IsOccupied() const;
+  /**
+   * @brief Returns a boolean value indicating if the cell is occupied or not.
+   *
+   * @return True if the cell is occupied, false otherwise
+   */
+  bool IsOccupied() const;
 
-    void Occupy(std::string type);
+  /**
+   * @brief Occupies the cell with an object of the type which is given as a
+   * parameter.
+   *
+   * @param type The type of the object being placed in the cell ("Horizontal
+   * road", "Vertical road", "Intersection", "industrial", "residential",
+   * "shop", "gym", or "restaurant")
+   */
+  void Occupy(std::string type);
 
-    void Clear();
+  /**
+   * @brief Empties the cell (I.e. sets the value of the type_ variable of the cell to "Empty")
+   * 
+   */
+  void Clear();
 
-    std::string GetType();
+  /**
+   * @brief Get the type of the cell.
+   * 
+   * @return The type of the cell as std::string 
+   */
+  std::string GetType() const;
 
-    int GetX(){
-        return location_.first;
-    }
+  /**
+   * @brief Get the x-coordinate of the cell.
+   * 
+   * @return The x-coordinate as an integer
+   */
+  int GetX() const;
 
-    int GetY(){
-        return location_.second;
-    }
+  /**
+   * @brief Get the y-coordinate of the cell.
+   * 
+   * @return The y-coordinate as an integer
+   */
+  int GetY() const;
 
-    void Draw(sf::RenderWindow& window, int cellSize, int x, int y);
+  /**
+   * @brief Draws the cell to the SFML window given as a parameter.
+   * 
+   * @param window A reference to the SFML window to draw the cell to
+   * @param cellSize The size of the cell to be drawn
+   * @param x The x-coordinate of the cell to be drawn
+   * @param y The y-coordinate of the cell to be drawn
+   */
+  void Draw(sf::RenderWindow& window, int cellSize, int x, int y);
 
-private:
-
-   std::string type_;
-   std::pair<int, int> location_;
-    
+ private:
+  std::string type_;
+  std::pair<int, int> location_;
 };
 
-
-#endif 
+#endif
