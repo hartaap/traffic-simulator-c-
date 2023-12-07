f90d13059bc843bab7c9a60507f7e707b3ac0926
Jiri Simell <jiri.simell@gmail.com>
2023-12-07 18:58:49 +0200

    Added documentation

--- src/building.cpp
+++ src/building.cpp
@@ -1,26 +1,28 @@
 #include "building.hpp"
 
-Building::Building(const std::string& buildingName, const std::pair<int, int>& location, const std::string& type)
-    : buildingName_(buildingName), location_(location), type_(type), personAmount_(0), carAmount_(0) {
-    
+Building::Building(const std::string& buildingName,
+                   const std::pair<int, int>& location, const std::string& type)
+    : buildingName_(buildingName),
+      location_(location),
+      personAmount_(0),
+      carAmount_(0),
+      type_(type)
+       {}
+
+Building::~Building() {}
+
+int Building::GetMaxPeopleCapacity() const { return maxPersonCapacity_; }
+
+int Building::GetMaxCarCapacity() const { return maxCarCapacity_; }
+
+int Building::GetPeopleAmount() const { return personAmount_; }
+
+int Building::GetCarAmount() const { return carAmount_; }
+
+bool Building::isFull() const {
+  return ((maxPersonCapacity_ - personAmount_) > 0);
 }
 
-Building::~Building() {
-}
+const std::string& Building::GetName() const { return buildingName_; }
 
-
-const std::string& Building::GetName() const {
-    return buildingName_;
-}
-
-const std::pair<int, int>& Building::GetLocation() const {
-    return location_;
-}
-
-int Building::GetPeopleAmount() const {
-        return personAmount_;
-}
-
-int Building::GetCarAmount() const {
-        return carAmount_;
-}
+const std::pair<int, int>& Building::GetLocation() const { return location_; }
