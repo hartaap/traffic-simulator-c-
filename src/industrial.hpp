bfa84f14d2be354cb9e16f6e52b7320663d53cea
Hartikainen Aape <aape.hartikainen@aalto.fi>
2023-12-11 00:11:08 +0200

    Updated industrial looks

--- src/industrial.hpp
+++ src/industrial.hpp
@@ -34,9 +34,29 @@ class Industrial : public Building {
     sf::RectangleShape cellShape(sf::Vector2f(cellSize, cellSize));
     int x = location_.first;
     int y = location_.second;
-    cellShape.setFillColor(sf::Color::Black);
+
+    cellShape.setFillColor(sf::Color(180, 180, 180)); 
     cellShape.setPosition(x * cellSize, y * cellSize);
+    cellShape.setSize(sf::Vector2f(cellSize, cellSize)); 
+
     window.draw(cellShape);
+
+    sf::RectangleShape door(sf::Vector2f(cellSize * 0.4f, cellSize));
+    door.setFillColor(sf::Color(120, 120, 120)); 
+    door.setPosition((x + 0.6f) * cellSize, y * cellSize);
+    window.draw(door);
+
+    for (int i = 0; i < 2; ++i) {
+        sf::RectangleShape windowRect(sf::Vector2f(cellSize * 0.2f, cellSize * 0.2f));
+        windowRect.setFillColor(sf::Color(200, 200, 200)); 
+        windowRect.setPosition((x + 0.2f + i * 0.4f) * cellSize, (y + 0.2f) * cellSize);
+        window.draw(windowRect);
+    }
+    sf::RectangleShape streamlinedFeature(sf::Vector2f(cellSize, cellSize * 0.1f));
+    streamlinedFeature.setFillColor(sf::Color(150, 150, 150));
+    streamlinedFeature.setPosition(x * cellSize, (y + 0.9f) * cellSize);
+    window.draw(streamlinedFeature);
+
   }
 
   /**
