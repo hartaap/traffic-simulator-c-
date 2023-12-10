ee4ec229124981e3657bda63cde11f3bf0435a8f
Hartikainen Aape <aape.hartikainen@aalto.fi>
2023-12-10 21:07:48 +0200

    Added gettype()

--- src/building.cpp
+++ src/building.cpp
@@ -1,15 +1,13 @@
 #include "building.hpp"
 
 Building::Building(const std::string& buildingName,
-                   const std::pair<int, int>& location, const std::string& type, Node* node)
+                   const std::pair<int, int>& location, const std::string& type)
     : buildingName_(buildingName),
       location_(location),
       personAmount_(0),
       carAmount_(0),
       type_(type)
-       {
-        node_ = node;
-       }
+       {}
 
 Building::~Building() {}
 
@@ -22,17 +20,10 @@ int Building::GetPeopleAmount() const { return personAmount_; }
 int Building::GetCarAmount() const { return carAmount_; }
 
 bool Building::isFull() const {
-  return ((maxPersonCapacity_ - personAmount_) > 0);
+  return ((maxPersonCapacity_ - personAmount_) <= 0);
 }
 
 const std::string& Building::GetName() const { return buildingName_; }
 
 const std::pair<int, int>& Building::GetLocation() const { return location_; }
 
-Node* Building::GetNodeFrom() {
-  if (node_ != nullptr) {
-    return node_;
-  } else {
-    std::cerr << "Unknown building!" << std::endl;
-  }
-}
