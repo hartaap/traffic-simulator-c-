258c9ff0081f4431b385d51169b4b940063c86fa
Hartikainen Aape <aape.hartikainen@aalto.fi>
2023-12-10 15:32:12 +0200

    Added node as a parameter and private member

--- src/industrial.hpp
+++ src/industrial.hpp
@@ -1,23 +1,6 @@
-#ifndef INDUSTRIAL_H
-#define INDUSTRIAL_H
-
-#include "building.hpp"
-
-/**
- * @brief One of the concretizing classes for the abstract class Building.
- *
- */
-class Industrial : public Building {
- public:
-  /**
-   * @brief Construct a new Industrial object
-   *
-   * @param buildingName The name of the industrial building
-   * @param location The coordinates of the idnustrial building
-   */
   Industrial(const std::string& buildingName,
-             const std::pair<int, int>& location)
-      : Building(buildingName, location, "Industrial") {
+             const std::pair<int, int>& location, Node* node)
+      : Building(buildingName, location, "Industrial", node) {
     maxPersonCapacity_ = 30;
     maxCarCapacity_ = 25;
   }
@@ -50,3 +33,4 @@ class Industrial : public Building {
 };
 
 #endif
+
