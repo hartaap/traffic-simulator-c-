d7876dd97cae4e8bd1936c38e15d79fedb499e35
aba <brander.artur@gmail.com>
2023-12-11 18:46:27 +0200

    added documentation

--- src/visualization.hpp
+++ src/visualization.hpp
@@ -8,14 +8,39 @@
 #include "car.hpp"
 #include "grid.hpp"
 
+/**
+ * @brief Class used for visualizing the traffic simulator in a separate GUI
+ *
+ */
 class Visualization {
  public:
+  /**
+   * @brief Construct a new Visualization object
+   *
+   * @param cellSize cell size in the GUI
+   * @param g pointer to a grid
+   */
   Visualization(int cellSize, Grid* g);
 
+  /**
+   * @brief Used for returning the Window object
+   * 
+   * @return sf::RenderWindow&
+   */
   sf::RenderWindow& GetWindow();
 
+  /**
+   * @brief Prints out the current grid
+   * 
+   * @param grid 
+   */
   void PrintGrid(Grid* grid);
 
+  /**
+   * @brief Prints out the current cars
+   * 
+   * @param cars 
+   */
   void PrintCars(std::vector<Car*> cars);
 
  private:
