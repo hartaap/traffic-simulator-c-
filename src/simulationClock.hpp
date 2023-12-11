4343d47f6536300ae02e20fe34c386ce23b25709
aba <brander.artur@gmail.com>
2023-12-11 12:54:05 +0200

    Fixed indentation

--- src/simulationClock.hpp
+++ src/simulationClock.hpp
@@ -2,8 +2,8 @@
 #define CLOCK_HPP
 
 #include <chrono>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include <sstream>
 
 /**
@@ -12,41 +12,42 @@
  * One second in real life equals one minute in simulation time.
  */
 class SimulationClock {
-public:
-    /**
-     * @brief Construct a new Simulation Clock object
-     */
-    SimulationClock();
+ public:
+  /**
+   * @brief Construct a new Simulation Clock object
+   */
+  SimulationClock();
 
-    /**
-     * @brief Function that starts the clock
-     */
-    void Start();
+  /**
+   * @brief Function that starts the clock
+   */
+  void Start();
 
-    /**
-     * @brief Returns the elapsed time in simulation
-     * 
-     * @return A double value that represents the elapsed time in seconds
-     */
-    double GetElapsedTime();
+  /**
+   * @brief Returns the elapsed time in simulation
+   *
+   * @return A double value that represents the elapsed time in seconds
+   */
+  double GetElapsedTime();
 
-    /**
-     * @brief Returns the day number of the simulation
-     * 
-     * @return An integer value that represents the day number
-     */
-    int GetDayNumber();
+  /**
+   * @brief Returns the day number of the simulation
+   *
+   * @return An integer value that represents the day number
+   */
+  int GetDayNumber();
 
-    /**
-     * @brief Returns the simulation's time (1 second in real life = 1 minute in simulation time)
-     * 
-     * @return A string value that contains the time in "xx:xx" form
-     */
-    std::string GetSimulationTime();
+  /**
+   * @brief Returns the simulation's time (1 second in real life = 1 minute in
+   * simulation time)
+   *
+   * @return A string value that contains the time in "xx:xx" form
+   */
+  std::string GetSimulationTime();
 
-private:
-    std::chrono::time_point<std::chrono::high_resolution_clock> startTime_;
-    int dayNumber_;
+ private:
+  std::chrono::time_point<std::chrono::high_resolution_clock> startTime_;
+  int dayNumber_;
 };
 
 #endif  // CLOCK_HPP
