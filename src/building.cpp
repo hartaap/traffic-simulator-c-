75c17e385947b6cd6c3a0c839076b8d1e0ee9811
Hartikainen Aape <aape.hartikainen@aalto.fi>
2023-12-11 23:10:12 +0200

    deleted unnecessary functions

--- src/building.cpp
+++ src/building.cpp
@@ -4,23 +4,9 @@ Building::Building(const std::string& buildingName,
                    const std::pair<int, int>& location, const std::string& type)
     : buildingName_(buildingName),
       location_(location),
-      personAmount_(0),
-      carAmount_(0),
       type_(type) {}
 
 Building::~Building() {}
-
-int Building::GetMaxPeopleCapacity() const { return maxPersonCapacity_; }
-
-int Building::GetMaxCarCapacity() const { return maxCarCapacity_; }
-
-int Building::GetPeopleAmount() const { return personAmount_; }
-
-int Building::GetCarAmount() const { return carAmount_; }
-
-bool Building::isFull() const {
-  return ((maxPersonCapacity_ - personAmount_) <= 0);
-}
 
 const std::string& Building::GetName() const { return buildingName_; }
 
