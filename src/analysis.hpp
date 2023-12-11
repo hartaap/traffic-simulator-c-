00de45e9d03f6aaa74cc8d78009f0d852ade8af6
aba <brander.artur@gmail.com>
2023-12-11 12:36:03 +0200

    Added documentation

--- src/analysis.hpp
+++ src/analysis.hpp
@@ -1,45 +1,82 @@
 #ifndef ANALYSIS_HPP
 #define ANALYSIS_HPP
 
+#include <algorithm>
+#include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
+
 #include "city.hpp"
 #include "simulationClock.hpp"
-#include <algorithm> 
-#include <iostream> 
-#include <fstream>
 
+/**
+ * @brief Class used for analyzing the traffic data
+ * provided by the simulator.
+ */
 class Analysis {
-public:
-    Analysis(City* city, SimulationClock* clock);
+ public:
+  /**
+   * @brief Construct a new Analysis object
+   *
+   * @param city Pointer to the main city object
+   * @param clock Pointer to the main clock object
+   */
+  Analysis(City* city, SimulationClock* clock);
 
-    Analysis();
+  /**
+   * @brief The main function of this class, which is used to obtain the
+   * analysis data
+   */
+  void Analyze();
 
-    // Main function of this class which is going to be run in the main loop
-    void Analyze();
+  /**
+   * @brief Specifies the road that is going to be analyzed
+   *
+   * @param roadIndex Index of the road that is going to be analyzed
+   */
+  void SpecifyRoad(int roadIndex);
 
-    // Specify a road to analyze; using a palikka implementation of roadIndex for now
-    void SpecifyRoad(int roadIndex);
+  /**
+   * @brief Prints out a histogram of the current day with hourly statistics of
+   * road usage
+   *
+   * @param data Road usage data that is provided by the GetData function in
+   * this class
+   */
+  void GenerateHourlyHistogram(std::vector<std::vector<int>> data);
 
-    // Histogram of amount of cars on the road in respect to hour of the day
-    void GenerateHourlyHistogram(std::vector<std::vector<int>> data);
+  /**
+   * @brief Exports the analysis data into a CSV file in the root folder of the
+   * project
+   *
+   * @param data Road usage data that is provided by the GetData function in
+   * this class
+   */
+  void ExportToCSV(const std::string& filename);
 
-    // Export the analysis results to a CSV file, could be changed to JSON but for now CSV since easier
-    void ExportToCSV(const std::string& filename);
+  /**
+   * @brief Returns the hourly road data
+   *
+   * @return The hourly road data
+   */
+  std::vector<std::vector<int>> GetData();
 
-    std::vector<std::vector<int>> GetData();
+  /**
+   * @brief Initializes the previousCars_ private variable
+   */
+  void Init();
 
-    void Init();
-private:
-    // Current road that is being analyzed
-    Road* currentRoad_;
+ private:
+  // Current road that is being analyzed
+  Road* currentRoad_;
 
-    // first element tells the day, second element tells the hour
-    std::vector<std::vector<int>> roadHourlyCounts_;
+  // first element tells the day, second element tells the hour
+  std::vector<std::vector<int>> roadHourlyCounts_;
 
-    City* city_;
-    SimulationClock* clock_;
-    std::map<int, bool> previousCars_;
+  City* city_;
+  SimulationClock* clock_;
+  std::map<int, bool> previousCars_;
 };
 
 #endif  // ANALYSIS_HPP
