845ed29c6594f4b19b3f8d94ab2a26d171389000
Jiri Simell <jiri.simell@gmail.com>
2023-12-11 20:48:59 +0200

    Code styling

--- src/commercial.hpp
+++ src/commercial.hpp
@@ -46,5 +46,3 @@ class Commercial : public Building {
 };
 
 #endif
-
-
--- src/event.hpp
+++ src/event.hpp
@@ -1,8 +1,9 @@
 #ifndef EVENT_H
 #define EVENT_H
+#include <random>
+
 #include "node.hpp"
 #include "person.hpp"
-#include <random>
 
 /**
  * @brief The Event class is responsible for creating random schedules
@@ -12,28 +13,27 @@
 
 class Event {
  public:
-   /**
+  /**
    * @brief Construct a new Event object.
    *
    * @param person The person whose schedule is created.
    * @param buildingNodes The nodes that contain a building.
    */
-   Event(Person* person, std::vector<Node*> buildingNodes);
+  Event(Person* person, std::vector<Node*> buildingNodes);
 
-   /**
+  /**
    * @brief Create a new random schedule for a person.
    *
    * @return Returns the schedule in map format, where a time value is
    * mapped to a corresponding event node.
    */
-   std::map<int, Node*> CreateSchedule();
+  std::map<int, Node*> CreateSchedule();
 
  private:
-   std::map<int, Node*> schedule_;
-   Person* person_;
-   std::vector<Node*> buildingNodes_;
-   std::default_random_engine generator_; // for randomizing
+  std::map<int, Node*> schedule_;
+  Person* person_;
+  std::vector<Node*> buildingNodes_;
+  std::default_random_engine generator_;  // for randomizing
 };
-
 
 #endif
